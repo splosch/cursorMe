bf2441c1ef44a390bcfada2b77a85b2fd0318d71
splosch <sploscho@googlemail.com>
2013-08-18 22:18:00 +0200

    Make DragnDrop and reposion work aside

--- resources/cursorme.js
+++ resources/cursorme.js
@@ -1,35 +1,136 @@
-function drawImage(imageObj) { 
-  var stage = new Kinetic.Stage({
-    container: "container",
-    width: 578,
-    height: 200
-  });
-  var layer = new Kinetic.Layer();
+/*
+ *  Drag and drop an (external) image to the canvas
+ *  Save the result to an image
+ *  by Robert Nyman http://robertnyman.com 
+ */
 
-  // darth vader
-  var darthVaderImg = new Kinetic.Image({
-    image: imageObj,
-    x: stage.getWidth() / 2 - 200 / 2,
-    y: stage.getHeight() / 2 - 137 / 2,
-    width: 16,
-    height: 24,
-    draggable: true
-  });
+(function () {
+  var canvas = document.getElementById("my-canvas"),
+    context = canvas.getContext("2d"),
+    img = document.createElement("img"),
+    mouseDown = false,
+    brushColor = "rgb(0, 0, 0)",
+    hasText = true,
+    clearCanvas = function () {
+      if (hasText) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        hasText = false;
+      }
+    };
+    
+  // Adding instructions
+  context.fillText("Drop an image onto the canvas", 240, 200);
+  context.fillText("Click a spot to set as brush color", 240, 220);
+  
+  // Image for loading  
+  img.addEventListener("load", function () {
+    clearCanvas();
+    context.drawImage(img, 0, 0);
+  }, false);
+  
+  // Detect mousedown
+  canvas.addEventListener("mousedown", function (evt) {
+    clearCanvas();
+    mouseDown = true;
+    context.beginPath();
+  }, false);
+  
+  // Detect mouseup
+  canvas.addEventListener("mouseup", function (evt) {
+    mouseDown = false;
+    var colors = context.getImageData(evt.layerX, evt.layerY, 1, 1).data;
+    brushColor = "rgb(" + colors[0] + ", " + colors[1] + ", " + colors[2] + ")";
+  }, false);
+  
+  // Draw, if mouse button is pressed
+  canvas.addEventListener("mousemove", function (evt) {
+    if (mouseDown) {
+      context.strokeStyle = brushColor;               
+      context.lineWidth = 20;
+      context.lineJoin = "round";
+      context.lineTo(evt.layerX+1, evt.layerY+1);
+      context.stroke();
+    }
+  }, false);
 
-  // add cursor styling
-  darthVaderImg.on('mouseover', function() {
-    document.body.style.cursor = 'pointer';
-  });
-  darthVaderImg.on('mouseout', function() {
-    document.body.style.cursor = 'default';
-  });
+  // To enable drag and drop
+  canvas.addEventListener("dragover", function (evt) {
+    evt.preventDefault();
+  }, false);
 
-  layer.add(darthVaderImg);
-  stage.add(layer);
+  // Handle dropped image file - only Firefox and Google Chrome
+  canvas.addEventListener("drop", function (evt) {
+    var files = evt.dataTransfer.files;
+    if (files.length > 0) {
+      var file = files[0];
+      if (typeof FileReader !== "undefined" && file.type.indexOf("image") != -1) {
+        var reader = new FileReader();
+        // Note: addEventListener doesn't work in Google Chrome for this event
+        reader.onload = function (evt) {
+          img.src = evt.target.result;
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+    evt.preventDefault();
+  }, false);
+  
+  // Save image
+  var saveImage = document.createElement("button");
+  saveImage.innerHTML = "Save canvas";
+  saveImage.addEventListener("click", function (evt) {
+    window.open(canvas.toDataURL("image/png"));
+    evt.preventDefault();
+  }, false);
+  document.getElementById("myCoursoredImg").appendChild(saveImage);
+})();
+
+
+/*
+ *  Reposition an image inside a Cnavas
+ *
+ *
+ */
+(function () {
+  function drawImage(imageObj) { 
+    var stage = new Kinetic.Stage({
+      container: "container",
+      width: 578,
+      height: 200
+    });
+    var layer = new Kinetic.Layer();
+
+    // darth vader
+    var darthVaderImg = new Kinetic.Image({
+      image: imageObj,
+      x: stage.getWidth() / 2 - 200 / 2,
+      y: stage.getHeight() / 2 - 137 / 2,
+      width: 16,
+      height: 24,
+      draggable: true
+    });
+
+    // add cursor styling
+    darthVaderImg.on('mouseover', function() {
+      document.body.style.cursor = 'pointer';
+    });
+    darthVaderImg.on('mouseout', function() {
+      document.body.style.cursor = 'default';
+    });
+
+    layer.add(darthVaderImg);
+    stage.add(layer);
   }
+
+
   var imageObj = new Image();
+
   imageObj.onload = function() {
-  drawImage(this);
+    drawImage(this);
   };
-  imageObj.src = 'cursorImg/hand1.png';
-}
+
+  imageObj.src = 'resources/cursorImg/hand1.png';
+})();
+
+
+
