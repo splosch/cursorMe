cccca13b368282d162dcba6111fc199b35257942
splosch <sploscho@googlemail.com>
2015-02-21 21:27:03 +0100

    Setup jasmin and js-imagediff for basic feature testing * include sample "cursor" images and license info * prepare js-imagediff test

--- assets/javascript/test/cursorMe_spec.js
+++ assets/javascript/test/cursorMe_spec.js
@@ -1,13 +1,77 @@
-describe("CursorMe", function() {
-  var cursorMe,
-      $stage = $("#my_stage");
+/*
+ *
+ *
+ * uses the image "Cursor" by Jeff Warren (https://www.flickr.com/photos/jeffreywarren/2970122251)
+ * licenced under (CC BY-SA 2.0) --- https://creativecommons.org/licenses/by-sa/2.0
+ */
 
+var myStage,
+    STAGE_SEL = "#my-canvas",
+    $stage = $('<canvas id="my-canvas" width="600" height="400">I am canvas</canvas>');
+
+describe("Basic cursorMe instantiation", function() {
   beforeEach(function() {
-    //stage = $.cursorMe($stage, {});
+    $("body").append($stage);
   });
 
-  it("should add cursorMe to the jQuery namespace", function() {
-    expect(typeof $.cursorMe).toEqual("function");
+  afterEach(function() {
+    $(STAGE_SEL).remove();
   });
 
+  it("should create an instance of cursorMe", function() {
+    var myStage = $.cursorMe($(STAGE_SEL), {});
+    expect(typeof myStage).toEqual("object");
+  });
 });
+
+describe("Interacting with cursorMe API", function() {
+  beforeEach(function() {
+    var newBackgroundImage = new Image(),
+        cursoMeResult      = new Image();
+
+    this.addMatchers(imagediff.jasmine);
+
+    $("body").append($stage);
+    myStage = $.cursorMe($(STAGE_SEL), {});
+
+    newBackgroundImage.src = 'images/cursor_150x150.jpg';
+    myStage.setBackground(newBackgroundImage);
+    newBackgroundImage.complete(done);
+  });
+
+  afterEach(function() {
+    $(STAGE_SEL).remove();
+  });
+
+  xit('should convert be the same image', function () {
+    expect(newBackgroundImage).toImageDiffEqual(cursoMeResult);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ assets/javascript/test/images/license.md
@@ -0,0 +1,5 @@
+The pictures used to show the functionality of "cursorMe" are taken from flickr under the following license:
+Attribution-ShareAlike 2.0 Generic (CC BY-SA 2.0) --- https://creativecommons.org/licenses/by-sa/2.0
+
+The image:
+* "Cursor" by Jeff Warren (https://www.flickr.com/photos/jeffreywarren/2970122251)
