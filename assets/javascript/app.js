8a78e1c387e14b3e8bc2e893a64dbcd9bdd7f01a
splosch <sploscho@googlemail.com>
2015-02-23 20:55:18 +0100

    Highlight selected cursor and allow to reset cursor

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,19 +2,19 @@
   "use strict";
 
   var //backImg     = "assets/images/IMG_7791.JPG",
-  	  backImg     = "assets/javascript/test/images/cursor_150x150.jpg",
-  	  pointerImg  = $("input[type='radio'][name='cursortype']:checked").siblings("img").attr("src") || "assets/images/icons/hand1.png",
+      backImg     = "assets/javascript/test/images/cursor_150x150.jpg",
+      pointerImg  = $("input[type='radio'][name='cursortype']:checked + label").find("img").attr("src") || "assets/images/icons/hand1.png",
       $container  = $("#container"),
       $myCursorMe = $.cursorMe($container);
 
   $myCursorMe.setBackground(backImg);
 
   // handle setting the cursor
- 	$myCursorMe.setPointer(pointerImg);
+  $myCursorMe.setPointer(pointerImg);
   $("input[type='radio'][name='cursortype']").on("change", function(event, element){
-  	var newPointerImg  = $(this).siblings("img").attr("src") || pointerImg;
+    var newPointerImg  = $(this).siblings("label").find("img").attr("src") || pointerImg;
 
-  	$myCursorMe.setPointer(newPointerImg);
+    $myCursorMe.setPointer(newPointerImg);
   });
 
 
